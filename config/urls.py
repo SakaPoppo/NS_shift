30ae6efcd6373391f057b9cb02db71cfe75af942
ryota orikasa <>
2026-07-11 23:33:02 +0900

    ルーティング、URLsの設定

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     path("shifts/", include("shifts.urls")),
     path("", include("core.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
